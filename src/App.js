16a4814fa5592894cc3003864e37f64e02595040
alanmontgomery <alan.montgomery@tascomi.com>
2021-04-29 09:54:48 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
